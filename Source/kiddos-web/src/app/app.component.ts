b0e6847f6e068af438ca4bad057eed8319d03f42
Jan Willem Fransen <fransen@bataviagroep.nl>
2018-07-07 16:59:18 +0200

    Volgens: https://medium.com/@tomastrajan/how-to-build-responsive-layouts-with-bootstrap-4-and-angular-6-cfbb108d797b

---
+++ Source/kiddos-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kiddos-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'kiddos';
+}
