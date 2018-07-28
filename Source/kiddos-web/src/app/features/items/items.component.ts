5dcdf8b42a4988552613fbb0995a34c244fb693b
Jan Willem fransen <fransen@bataviagroep.nl>
2018-07-28 17:53:05 +0200

    .

--- Source/kiddos-web/src/app/features/items/items.component.ts
+++ Source/kiddos-web/src/app/features/items/items.component.ts
@@ -17,35 +17,48 @@ export class ItemsComponent implements OnInit {
   constructor(private itemService: ItemService) { }
 
   ngOnInit() {
-    this.getItem();
+    this.getRandom();
   }
 
-  getItem(): void {
+  getVolgende(): void {
+    this.getItem(1);
+  }
+
+  getVorige(): void {
+    this.getItem(-1);
+  }
+
+  getRandom(): void {
+    this.teller = -1;
+    this.getItem(1);
+  }
+
+  getItem(index): void {
     this.items = [];
 
     this.itemService.getItems()
     .subscribe(
       data => {
-        this.getTeller(data.length);
+        this.getTeller(data.length, index);
         this.items.push(data[this.teller]);
       },
       error => console.log(error)
     )
   }
 
-  getTeller(maximaalAantal) {
+  getTeller(maximaalAantal, index) {
     if(this.teller == -1) {
-      this.getRandom(maximaalAantal)
+      this.getRandomVanAantalItems(maximaalAantal)
     }
 
-    this.teller = this.teller + 1;
+    this.teller = this.teller + index;
 
     if( this.teller == maximaalAantal.length ) {
       this.teller = 0;
     }
   }
 
-  getRandom(maximaalAantal) {
+  getRandomVanAantalItems(maximaalAantal) {
     this.teller = Math.floor(Math.random() * maximaalAantal);
   }
 }
