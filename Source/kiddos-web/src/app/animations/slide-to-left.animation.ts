c495d0d2c4d6400444840d76157e5b5e85060778
Jan Willem Fransen <fransen@bataviagroep.nl>
2018-12-29 23:12:30 +0100

    Animatie doet het eindelijk fatsoenlijke

--- Source/kiddos-web/src/app/animations/slide-to-left.animation.ts
+++ Source/kiddos-web/src/app/animations/slide-to-left.animation.ts
@@ -6,10 +6,10 @@ export const slideToLeft =
     state('*', style({ position: 'fixed', width: '100%' })),
     transition(':enter', [  // before 2.1: transition('void => *', [
       style({ transform: 'translateX(100%)' }),
-      animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' }))
+      animate('0.1s ease-in-out', style({ transform: 'translateX(0%)' }))
     ]),
     transition(':leave', [  // before 2.1: transition('* => void', [
       style({ transform: 'translateX(0%)' }),
-      animate('0.5s ease-in-out', style({ transform: 'translateX(-100%)' }))
+      animate('0.1s ease-in-out', style({ transform: 'translateX(-100%)' }))
     ])
   ]);
--- Source/kiddos-web/src/app/app-routing.module.ts
+++ Source/kiddos-web/src/app/app-routing.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, RouteReuseStrategy } from '@angular/router';
 
+import { CustomReuseStrategy } from './routing/route-reuse-strategy';
 import { ItemsComponent } from './features/items/items.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'items',  pathMatch: 'full' },
   { path: 'items', component: ItemsComponent },
-  { path: 'items/:id', component: ItemsComponent },
-  { path: 'items/:id/:direction', component: ItemsComponent }
+  { path: 'items/:id', component: ItemsComponent, data: { state: 'slideToLeft' } },
+  { path: 'items/:id/:direction', component: ItemsComponent, data: { state: 'slideToLeft' } }
 ];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
+  providers: [{
+    provide: RouteReuseStrategy,
+    useClass: CustomReuseStrategy
+  }],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
